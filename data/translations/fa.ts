41ca91ef5064d0413a0af141a21c0f97c8bc35dc
goodosuser <36658549+goodosuser@users.noreply.github.com>
2021-03-29 07:42:47 +0430

    add new translation file
    اضافه کردن ترجمه فارسی

---
+++ data/translations/fa.ts
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fa">
+<context>
+    <name>PictureBox</name>
+    <message>
+        <source>Press to login</source>
+        <translation>برای ورود بزنید</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>%1 (Wayland)</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>TextConstants</name>
+    <message>
+        <source>Welcome to %1</source>
+        <translation>خوش آمدید به %1</translation>
+    </message>
+    <message>
+        <source>Warning, Caps Lock is ON!</source>
+        <translatorcomment>قفل  حروف صفحه کلید روشن است</translatorcomment>
+        <translation>هشدار Caps Lock روشن است!</translation>
+    </message>
+    <message>
+        <source>Layout</source>
+        <translation>چیدمان</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>ورود</translation>
+    </message>
+    <message>
+        <source>Login failed</source>
+        <translation>ورود با شکست مواجه شد</translation>
+    </message>
+    <message>
+        <source>Login succeeded</source>
+        <translation>ورود موفقیت آمیز بود</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>کلمه عبور</translation>
+    </message>
+    <message>
+        <source>Enter your username and password</source>
+        <translation>نام کاربری و کلمه عبورتان را وارد کنید</translation>
+    </message>
+    <message>
+        <source>Reboot</source>
+        <translatorcomment>خاموش روشن شدن </translatorcomment>
+        <translation>بازآغازی</translation>
+    </message>
+    <message>
+        <source>Session</source>
+        <translation>نشست</translation>
+    </message>
+    <message>
+        <source>Shutdown</source>
+        <translation>خاموش کردن</translation>
+    </message>
+    <message>
+        <source>User name</source>
+        <translation>نام کاربری</translation>
+    </message>
+    <message>
+        <source>Select your user and enter password</source>
+        <translation>کاربر خود را انتخاب و رمز را وارد کنید</translation>
+    </message>
+</context>
+</TS>
